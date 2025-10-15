e1ab55b9436b5cd95988e1c033e66f5e265d9b9a
lucasMN0 <lucasmn04@gmail.com>
2025-10-15 10:46:23 -0300

    Django presente

--- back-end/usuarios/admin.py
+++ back-end/usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Transacao, MetaFinanceira, Lembrete
 
-# Register your models here.
+admin.site.register(Transacao)
+admin.site.register(MetaFinanceira)
+admin.site.register(Lembrete)
--- back-end/usuarios/models.py
+++ back-end/usuarios/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Transacao(models.Model):
+    TIPO_CHOICES = [
+        ('expense', 'Gasto fixo'),
+        ('extra', 'Gasto extra'),
+        ('income', 'Ganho fixo'),
+        ('investment', 'Ganho extra'),
+        ('other', 'Outro'),
+    ]
+
+    data = models.DateField()
+    descricao = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.descricao} - {self.valor}"
+
+
+class MetaFinanceira(models.Model):
+    nome = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    concluida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Lembrete(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nome
